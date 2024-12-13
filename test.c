3f5eb58e88698c6e6b5897acb0a936c127f79bce
yanze <745675318@qq.com>
2024-12-13 23:30:39 +0800

    Tetris-20241213
    1.bricks can fall by each 1 second.
    2.developing the logic of when put next block in game area.
    3.modified code structure and style.
    
    Signed-off-by: yanze <745675318@qq.com>

--- test.c
+++ test.c
@@ -8,135 +8,149 @@
 #define BRICK   0x2593
 #define FRAME   0x2588
 
+#define ALL_AREA_COLOUN 40
+#define SIDE_AREA_COLOUN 16
+#define GAME_AREA_COLOUN 23
+#define GAME_AREA_ROW 21
+#define UP_AREA_ROW 6
+#define DOWN_AREA_ROW 14
+#define GAME_AREA_CENTER_COLOUN 5
+#define NEXT_AREA_ROW 11
+#define NEXT_AREA_COLOUN 14
+
+
 #define SITE_H 10
 #define SITE_V 20
+
+struct Block
+{
+	int space[4][4];
+}block[7][4];
+
+int game_area_sign[GAME_AREA_ROW][GAME_AREA_COLOUN] = {0};  //for further design
 
 void move_cursor(int row, int col) 
 {
     wprintf(L"\033[%d;%dH", row, col);
 }
 
-void print_shape_I(int row, int col)
+void DrawSpace(int shape, int form, int x, int y)
 {
-    move_cursor(row, col);
-    wprintf(L"%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 2, col);
-    wprintf(L"%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 3, col);
-    wprintf(L"%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (block[shape][form].space[i][j] == 1) //if need to clean block of a position, move cursor to it.
+			{
+				move_cursor(x + i, 2 * (y + j));
+				wprintf(L"  ");
+			}
+		}
+	}
 }
 
-void print_shape_O(int row, int col)
+void DrawBlock(int shape, int form, int x, int y)
 {
-    move_cursor(row, col);
-    wprintf(L"%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (block[shape][form].space[i][j] == 1) //if need to draw block of a position, move cursor to it.
+			{
+				move_cursor(x + i, 2 * (y + j));
+				wprintf(L"%lc%lc", BRICK, BRICK);
+			}
+		}
+	}
 }
 
-void print_shape_T(int row, int col)
+void Get_next_form(struct Block *src, struct Block *dest)
 {
-    move_cursor(row, col);
-    wprintf(L"%lc%lc%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"  %lc%lc  \n", BRICK, BRICK);
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4; j++) {
+            dest->space[j][3 - i] = src->space[i][j];
+        }
+    }
 }
 
-void print_shape_L(int row, int col)
+void InitBlockInfo()
 {
-    move_cursor(row, col);
-    wprintf(L"%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 2, col);
-    wprintf(L"%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-}
+	//“T” type
+	for (int i = 0; i <= 2; i++)
+		block[0][0].space[1][i] = 1;
+	block[0][0].space[2][1] = 1;
 
-void print_shape_J(int row, int col)
-{
-    move_cursor(row, col);
-    wprintf(L"  %lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"  %lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 2, col);
-    wprintf(L"%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-}
+	//“L” type
+	for (int i = 1; i <= 3; i++)
+		block[1][0].space[i][1] = 1;
+	block[1][0].space[3][2] = 1;
 
-void print_shape_Z(int row, int col)
-{
-    move_cursor(row, col);
-    wprintf(L"  %lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-}
+	//“J” type
+	for (int i = 1; i <= 3; i++)
+		block[2][0].space[i][2] = 1;
+	block[2][0].space[3][1] = 1;
 
-void print_shape_S(int row, int col)
-{
-    move_cursor(row, col);
-    wprintf(L"%lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
-    move_cursor(row + 1, col);
-    wprintf(L"  %lc%lc%lc%lc\n", BRICK, BRICK, BRICK, BRICK);
+	for (int i = 0; i <= 1; i++)
+	{
+		//“Z” type
+		block[3][0].space[1][i] = 1;
+		block[3][0].space[2][i + 1] = 1;
+		//“S” type
+		block[4][0].space[1][i + 1] = 1;
+		block[4][0].space[2][i] = 1;
+		//“O” type
+		block[5][0].space[1][i + 1] = 1;
+		block[5][0].space[2][i + 1] = 1;
+	}
+
+	//“I” type
+	for (int i = 0; i <= 3;i++)
+		block[6][0].space[i][1] = 1;
+
+	int temp[4][4];
+	for (int shape = 0; shape < 7; shape++) //7 types
+	{
+		for (int form = 0; form < 3; form++) //4 shapes
+		{
+			Get_next_form(&block[shape][form], &block[shape][form+1]);
+		}
+	}
 }
 
 
 
 void clear_next_area(void)
 {
-    for (int i = 0; i < 14; i++)
+    for (int i = UP_AREA_ROW + 3; i < GAME_AREA_ROW; i++)
     {
-        move_cursor(i + 9, 25);
+        move_cursor(i, GAME_AREA_COLOUN + 2);
         wprintf(L"\033[K");
         wprintf(L"              %lc%lc", FRAME, FRAME);
     }
 }
 
-int show_next_area(void)
+int show_next_area(int shpae_index)
 {
-    int random_number = rand() % 7;
-    switch (random_number) {
-        case 0:
-            print_shape_I(15, 30);
-            break;
-        case 1:
-            print_shape_O(15, 30);
-            break;
-        case 2:
-            print_shape_T(15, 30);
-            break;
-        case 3:
-            print_shape_L(15, 30);
-            break;
-        case 4:
-            print_shape_J(15, 30);
-            break;
-        case 5:
-            print_shape_Z(15, 30);
-            break;
-        default:
-            print_shape_S(15, 30);
-            break;
-    }
-
-    return random_number;
+    DrawBlock(shpae_index, 0, NEXT_AREA_ROW, NEXT_AREA_COLOUN);
 }
 
-void print_site(void)
+void print_site(void)  //print all areas
 {
     int i = 0;
-    for (i = 0; i < 40; i++)
+    for (i = 0; i < ALL_AREA_COLOUN; i++)
         wprintf(L"%lc", FRAME);
     wprintf(L"\n");
-    for (i = 0; i < 6; i++)
+    for (i = 0; i < UP_AREA_ROW; i++)
         wprintf(L"%lc%lc                    %lc%lc              %lc%lc\n", FRAME, FRAME, FRAME, FRAME, FRAME, FRAME);
     wprintf(L"%lc%lc                    %lc%lc", FRAME, FRAME, FRAME, FRAME);
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < SIDE_AREA_COLOUN; i++)
         wprintf(L"%lc", FRAME);
     wprintf(L"\n");    
-    for (i = 0; i < 14; i++)
+    for (i = 0; i < DOWN_AREA_ROW; i++)
         wprintf(L"%lc%lc                    %lc%lc              %lc%lc\n", FRAME, FRAME, FRAME, FRAME, FRAME, FRAME);        
-    for (i = 0; i < 40; i++)
+    for (i = 0; i < ALL_AREA_COLOUN; i++)
         wprintf(L"%lc", FRAME);
     wprintf(L"\n");
 }
@@ -149,17 +163,43 @@ void clear_line(int s, int e)
     wprintf(L"\033[%d;%dH", s, 1);
 }
 
-int main() {
-    int next_block_index = 0;
 
+int main() {
+    int block_index = 0;
+    int next_block_index = 0;
+    int block_row_position = 1;
+
+    int bottom_position = GAME_AREA_ROW - 1;  //temporary condition and parameter. waiting for further design.
+
+    InitBlockInfo();
+    setbuf(stdout,NULL);  //print in real time
     setlocale(LC_ALL, "");
     clear_line(1, 30);
     print_site();
     srand(time(NULL));
+    block_index = rand() % 7;
+
     while(1)
     {
-        next_block_index = show_next_area();
-        sleep(2);
+        DrawBlock(block_index, 0, block_row_position, GAME_AREA_CENTER_COLOUN);
+        next_block_index = rand() % 7;
+        show_next_area(next_block_index);
+        while(1)
+        {
+            sleep(1);
+            DrawSpace(block_index, 0, block_row_position, GAME_AREA_CENTER_COLOUN);
+            block_row_position ++;
+            DrawBlock(block_index, 0, block_row_position, GAME_AREA_CENTER_COLOUN);
+
+            if (block_row_position >= bottom_position)  //temporary condition and parameter. waiting for further design.
+            {
+                bottom_position = bottom_position - 2;
+                block_index = next_block_index;
+                block_row_position = 1;
+                break;
+            }
+        }
+        
         clear_next_area();
     }
 
